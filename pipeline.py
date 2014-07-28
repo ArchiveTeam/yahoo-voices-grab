5037d24068197d1a5daaafe999f23ff4e9f93b2b
Christopher Foo <chris.foo@gmail.com>
2014-07-28 11:53:02 -0400

    Add WIP pipeline

---
+++ pipeline.py
@@ -0,0 +1,255 @@
+# encoding=utf8
+import datetime
+from distutils.version import StrictVersion
+import hashlib
+import os.path
+import seesaw
+from seesaw.config import realize, NumberConfigValue
+from seesaw.externalprocess import WgetDownload
+from seesaw.item import ItemInterpolation, ItemValue
+from seesaw.pipeline import Pipeline
+from seesaw.project import Project
+from seesaw.task import SimpleTask, LimitConcurrent
+from seesaw.tracker import GetItemFromTracker, PrepareStatsForTracker, \
+    UploadWithTracker, SendDoneToTracker
+from seesaw.util import find_executable
+import shutil
+import socket
+import sys
+import time
+
+
+# check the seesaw version
+if StrictVersion(seesaw.__version__) < StrictVersion("0.1.5"):
+    raise Exception("This pipeline needs seesaw version 0.1.5 or higher.")
+
+
+###########################################################################
+# Find a useful Wget+Lua executable.
+#
+# WGET_LUA will be set to the first path that
+# 1. does not crash with --version, and
+# 2. prints the required version string
+WGET_LUA = find_executable(
+    "Wget+Lua",
+    ["GNU Wget 1.14.lua.20130523-9a5c"],
+    [
+        "./wget-lua",
+        "./wget-lua-warrior",
+        "./wget-lua-local",
+        "../wget-lua",
+        "../../wget-lua",
+        "/home/warrior/wget-lua",
+        "/usr/bin/wget-lua"
+    ]
+)
+
+if not WGET_LUA:
+    raise Exception("No usable Wget+Lua found.")
+
+
+###########################################################################
+# The version number of this pipeline definition.
+#
+# Update this each time you make a non-cosmetic change.
+# It will be added to the WARC files and reported to the tracker.
+VERSION = "20140827.50"
+USER_AGENT = 'ArchiveTeam'
+TRACKER_ID = 'yahoovoices'
+# TRACKER_HOST = 'tracker.archiveteam.org'
+TRACKER_HOST = 'localhost:9080'
+
+
+###########################################################################
+# This section defines project-specific tasks.
+#
+# Simple tasks (tasks that do not need any concurrency) are based on the
+# SimpleTask class and have a process(item) method that is called for
+# each item.
+class CheckIP(SimpleTask):
+    def __init__(self):
+        SimpleTask.__init__(self, "CheckIP")
+
+    def process(self, item):
+        # NEW for 2014! Check if we are behind firewall/proxy
+        ip_str = socket.gethostbyname('voices.yahoo.com')
+
+        if not ip_str.startswith('98.138.'):
+            item.log_output('Got IP address: %s' % ip_str)
+            item.log_output(
+                'Are you behind a firewall/proxy? That is a big no-no!')
+            raise Exception(
+                'Are you behind a firewall/proxy? That is a big no-no!')
+
+
+class PrepareDirectories(SimpleTask):
+    def __init__(self, warc_prefix):
+        SimpleTask.__init__(self, "PrepareDirectories")
+        self.warc_prefix = warc_prefix
+
+    def process(self, item):
+        item_name = item["item_name"]
+        escaped_item_name = item_name.encode('hex')
+        dirname = "/".join((item["data_dir"], escaped_item_name))
+
+        if os.path.isdir(dirname):
+            shutil.rmtree(dirname)
+
+        os.makedirs(dirname)
+
+        item["item_dir"] = dirname
+        item["warc_file_base"] = "%s-%s-%s" % (self.warc_prefix, escaped_item_name,
+            time.strftime("%Y%m%d-%H%M%S"))
+
+        open("%(item_dir)s/%(warc_file_base)s.warc.gz" % item, "w").close()
+
+
+class MoveFiles(SimpleTask):
+    def __init__(self):
+        SimpleTask.__init__(self, "MoveFiles")
+
+    def process(self, item):
+        # NEW for 2014! Check if wget was compiled with zlib support
+        if os.path.exists("%(item_dir)s/%(warc_file_base)s.warc"):
+            raise Exception('Please compile wget with zlib support!')
+
+        os.rename("%(item_dir)s/%(warc_file_base)s.warc.gz" % item,
+              "%(data_dir)s/%(warc_file_base)s.warc.gz" % item)
+
+        shutil.rmtree("%(item_dir)s" % item)
+
+
+def get_hash(filename):
+    with open(filename, 'rb') as in_file:
+        return hashlib.sha1(in_file.read()).hexdigest()
+
+
+CWD = os.getcwd()
+PIPELINE_SHA1 = get_hash(os.path.join(CWD, 'pipeline.py'))
+LUA_SHA1 = get_hash(os.path.join(CWD, 'yahoovoices.lua'))
+
+
+def stats_id_function(item):
+    # NEW for 2014! Some accountability hashes and stats.
+    d = {
+        'pipeline_hash': PIPELINE_SHA1,
+        'lua_hash': LUA_SHA1,
+        'python_version': sys.version,
+    }
+
+    return d
+
+
+class WgetArgs(object):
+    def realize(self, item):
+        wget_args = [
+            WGET_LUA,
+            "-U", USER_AGENT,
+            "-nv",
+            "--lua-script", "yahoovoices.lua",
+            "-o", ItemInterpolation("%(item_dir)s/wget.log"),
+            "--no-check-certificate",
+            "--output-document", ItemInterpolation("%(item_dir)s/wget.tmp"),
+            "--truncate-output",
+            "-e", "robots=off",
+            "--no-cookies",
+            "--rotate-dns",
+            # "--recursive", "--level=inf",
+            "--page-requisites",
+            "--timeout", "60",
+            "--tries", "inf",
+            "--span-hosts",
+            "--waitretry", "3600",
+            "--domains", "yahoo.com,yimg.com",
+            "--warc-file",
+                ItemInterpolation("%(item_dir)s/%(warc_file_base)s"),
+            "--warc-header", "operator: Archive Team",
+            "--warc-header", "yahoovoices-dld-script-version: " + VERSION,
+            "--warc-header", ItemInterpolation("yahoovoices-user: %(item_name)s"),
+        ]
+
+        item_name = item['item_name']
+        item_type, item_values = item_name.split(':')
+        item_values = item_values.split(',')
+        
+        assert item_type in ('contrib', 'article')
+        
+        if item_type == 'contrib':
+            template = 'http://contributor.yahoo.com/user/{0}.html'
+        else:
+            template = 'http://voices.yahoo.com/{0}.html'
+        
+        for value in item_values:
+            wget_args.append(template.format(value))
+
+        if 'bind_address' in globals():
+            wget_args.extend(['--bind-address', globals()['bind_address']])
+            print('')
+            print('*** Wget will bind address at {0} ***'.format(
+                globals()['bind_address']))
+            print('')
+
+        return realize(wget_args, item)
+
+
+###########################################################################
+# Initialize the project.
+#
+# This will be shown in the warrior management panel. The logo should not
+# be too big. The deadline is optional.
+project = Project(
+    title="Yahoo Voices",
+    project_html="""
+        <img class="project-logo" alt="Project logo" src="http://archiveteam.org/index.php?title=File:Yahoo-logo.png" height="50px" title="Don't change color, kitty. / Keep your color, kitty. / Stay that pretty gray."/>
+        <h2>Yahoo Voices <span class="links"><a href="http://voices.yahoo.com/">Website</a> &middot; <a href="http://tracker.archiveteam.org/yahoovoices/">Leaderboard</a></span></h2>
+        <p>Yahoo! is shutting down the contributor network.</p>
+    """,
+    utc_deadline=datetime.datetime(2014, 7, 30, 23, 58, 0)
+)
+
+pipeline = Pipeline(
+    CheckIP(),
+    GetItemFromTracker("http://%s/%s" % (TRACKER_HOST, TRACKER_ID), downloader,
+        VERSION),
+    PrepareDirectories(warc_prefix="yahoovoices"),
+    WgetDownload(
+        WgetArgs(),
+        max_tries=5,
+        accept_on_exit_code=[0, 8],
+        env={
+            "item_dir": ItemValue("item_dir")
+        }
+    ),
+    PrepareStatsForTracker(
+        defaults={"downloader": downloader, "version": VERSION},
+        file_groups={
+            "data": [
+                ItemInterpolation("%(item_dir)s/%(warc_file_base)s.warc.gz")
+            ]
+        },
+        id_function=stats_id_function,
+    ),
+    MoveFiles(),
+    LimitConcurrent(NumberConfigValue(min=1, max=4, default="1",
+        name="shared:rsync_threads", title="Rsync threads",
+        description="The maximum number of concurrent uploads."),
+        UploadWithTracker(
+            "http://%s/%s" % (TRACKER_HOST, TRACKER_ID),
+            downloader=downloader,
+            version=VERSION,
+            files=[
+                ItemInterpolation("%(data_dir)s/%(warc_file_base)s.warc.gz")
+            ],
+            rsync_target_source_path=ItemInterpolation("%(data_dir)s/"),
+            rsync_extra_args=[
+                "--recursive",
+                "--partial",
+                "--partial-dir", ".rsync-tmp"
+            ]
+            ),
+    ),
+    SendDoneToTracker(
+        tracker_url="http://%s/%s" % (TRACKER_HOST, TRACKER_ID),
+        stats=ItemValue("stats")
+    )
+)
