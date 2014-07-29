fa0117b97975a9b89a4e87cfbb2be42171487855
Christopher Foo <chris.foo@gmail.com>
2014-07-29 11:07:10 -0400

    Add index2items.py util

---
+++ utils/index2items.py
@@ -0,0 +1,40 @@
+'''Batch index URLs to tracker items.'''
+import argparse
+
+
+def main():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('file')
+    args = arg_parser.parse_args()
+
+    batch = set()
+
+    category = True
+
+    with open(args.file) as f:
+        for line in f:
+            name = line.strip().replace('http://voices.yahoo.com/', '').replace('.html', '')
+
+            if not name:
+                continue
+
+            assert ',' not in name
+            batch.add(name)
+
+            if len(batch) >= 10 or ('?cat' not in name and category):
+                if category:
+                    print('cat:' + ','.join(sorted(batch)))
+                else:
+                    print('index:' + ','.join(sorted(batch)))
+
+                batch.clear()
+
+                if '?cat' not in name and category:
+                    category = False
+
+    if batch:
+        print('article:' + ','.join(sorted(batch)))
+
+
+if __name__ == '__main__':
+    main()
